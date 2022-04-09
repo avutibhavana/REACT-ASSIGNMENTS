e9ed2b9915f941dd905b1c96f40f4705dd922799
avutibhavana <avutibhavana06@gmail.com>
2022-04-09 13:42:11 +0530

    assignment-4

--- Assignment-4/code/src/index.js
+++ Assignment-4/code/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Postview from './Postview';
+import Postview from './postview';
 import reportWebVitals from './reportWebVitals';
-
-
-
 
 
 ReactDOM.render(
